666314475e9a6a73738a69cdf8e37b382d20a42f
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2022-06-03 18:04:39 -0300

    Dar continuação no projeto :green_heart:

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Por Gabriel Carvalho</title>
+    <link rel="shortcut icon" href="/public/portfolio-icon-branco.png" type="image/x-icon">
+    <link rel="stylesheet" href="/public/css/homepage.css">
+    <link rel="stylesheet" href="/public/css/mobile-navbar.css">
+</head>
+<body>
+    <!-- Header mobile-navbar -->
+<header>
+      <nav>
+        <div class="logo-text">
+          <img src="/public/portfolio-icon-preto.png" alt="portfolio-icon-preto.png">
+          <div class="logo">
+            <p>Gabriel Carvalho</p>
+            <p class="dev-web">Desenvolvedor Web</p=>
+          </div>
+        </div>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main >
+
+    </main>
+    <article>
+        
+    </article>
+    <footer>
+
+    </footer>
+    <script src="/public/scripts/mobile-navbar.js"></script>
+</body>
+</html>

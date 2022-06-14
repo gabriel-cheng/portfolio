7be0f9561ae34ca70906ddb1931791bfd3e9da57
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2022-06-13 22:35:19 -0300

    Alterando app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const router = express.Router()
+const express = require("express");
+const app = express();
 
-app.use(express.static('public'))
+const port = process.env.PORT || 3000;
 
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'))
-})
+app.use(exoress.static('public'));
 
-app.use('/', router)
-app.listen(process.env.port || 3000)
+app.get("/", function(req, res) {
+    res.send(path.join(__dirname+'/index.html'));
+});
+
+app.listen(port, () => {
+    console.info("Aplicação rodando em http://localhost:3000");
+});

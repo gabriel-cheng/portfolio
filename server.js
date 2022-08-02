f988c8bd346f6e3815ece6f640b7aecacaa05efa
Gabriel Carvalho <gabriel_arvalho.contato@outlook.com>
2022-08-02 00:15:28 -0300

    Add a boolean in port const

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.set('view engine', 'handlebars');
 
 // Portas
 (function() {
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     app.listen(port, () => {
         console.log(`Server is running on port: ${port}`);
         console.log(`Access: http://localhost:${port}`);

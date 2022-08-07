c32703eb7d34e0c0ad2a2c2973681baae98d1189
Gabriel Carvalho <gabriel_arvalho.contato@outlook.com>
2022-08-07 14:38:53 -0300

    Correcting an script 'button-scale-effect' error :clap: :sparkles:

--- public/scripts/button-scale-effect-interval.js
+++ public/scripts/button-scale-effect-interval.js
@@ -2,21 +2,23 @@ const button1 = document.querySelector('.box-1-button-text');
 const button2 = document.querySelector('.box-2-button-text');
 const button3 = document.querySelector('.section-3-work');
 
-function intervalEffect(button) {
+if(button1, button2, button3) {
+    function intervalEffect(button) {
+        setInterval(() => {
+            setTimeout(() => {
+                button.classList.toggle('scaleInterval')
+            }, 500);
+            setTimeout(() => {
+                button.classList.toggle('scaleInterval')
+            }, 1200);
+        }, 6000);
+    };
+    
+    intervalEffect(button1);
+    intervalEffect(button2);
+    intervalEffect(button3);
+} else {
+    console.log('Itens não encontrados.');
 }
 
-function intervalEffect(button) {
-    setInterval(() => {
-        setTimeout(() => {
-            button.classList.toggle('scaleInterval')
-        }, 500);
-        setTimeout(() => {
-            button.classList.toggle('scaleInterval')
-        }, 1200);
-    }, 6000);
-};
-
-intervalEffect(button1);
-intervalEffect(button2);
-intervalEffect(button3);
 

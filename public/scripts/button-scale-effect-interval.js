f6d6402861cb404a3989aab930764eada5d75ba8
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-01-01 23:15:00 -0300

    refactor(master): adicionando eslint

--- public/scripts/button-scale-effect-interval.js
+++ public/scripts/button-scale-effect-interval.js
@@ -6,13 +6,13 @@ if(button1, button2, button3) {
     function intervalEffect(button) {
         setInterval(() => {
             setTimeout(() => {
-                button.classList.toggle('scaleInterval')
+                button.classList.toggle('scaleInterval');
             }, 500);
             setTimeout(() => {
-                button.classList.toggle('scaleInterval')
+                button.classList.toggle('scaleInterval');
             }, 1200);
         }, 6000);
-    };
+    }
     
     intervalEffect(button1);
     intervalEffect(button2);

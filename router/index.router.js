6010717199def8994e7410f542ae299f26ceb06d
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-01-02 01:13:11 -0300

    fix(prod): tentativa - 2 de correção

---
+++ router/index.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/index.controller');
+
+router.get('/to-view-project', controller.toViewProject);
+
+router.post('/congratulations', controller.congratulations);
+
+router.get('/sendmail', controller.sendMail);
+
+router.get('/projects', controller.projects);
+
+router.get('/about', controller.about);
+
+router.get('/contact-form', controller.contact);
+
+router.get('/', controller.index);
+
+module.exports = router;

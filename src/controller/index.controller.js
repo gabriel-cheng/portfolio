5a7f75cc12d1fc27047355262e506fb2e15bd267
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-02-23 01:28:40 -0300

    chore: adicionando blog em manutenção

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -2,7 +2,7 @@ const enviar = require('../../public/scripts/send-email.js');
 
 module.exports = {
     index: (req, res) => {
-        res.render('index');
+        res.render('maintenance');
     },
     contact: (req, res) => {
         res.render('contact-form');
--- src/router/index.router.js
+++ src/router/index.router.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/index.controller');
 
-router.get('/to-view-project', controller.toViewProject);
+// router.get('/to-view-project', controller.toViewProject);
 
-router.post('/congratulations', controller.congratulations);
+// router.post('/congratulations', controller.congratulations);
 
-router.get('/sendmail', controller.sendMail);
+// router.get('/sendmail', controller.sendMail);
 
-router.get('/projects', controller.projects);
+// router.get('/projects', controller.projects);
 
-router.get('/about', controller.about);
+// router.get('/about', controller.about);
 
-router.get('/contact-form', controller.contact);
+// router.get('/contact-form', controller.contact);
 
 router.get('/', controller.index);
 
